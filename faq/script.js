69ca2074beb226fff4940c898905378016b9a13b
Sakshi Bhandarkar <sakshi300699.b@gmail.com>
2021-11-01 17:43:40 +0530

    Update script.js

--- faq/script.js
+++ faq/script.js
@@ -1,18 +1,15 @@
-var faq = document.getElementsByClassName("faq-page");
-var i;
-
-for (i = 0; i < faq.length; i++) {
-    faq[i].addEventListener("click", function () {
-        /* Toggle between adding and removing the "active" class,
-        to highlight the button that controls the panel */
-        this.classList.toggle("active");
-
-        /* Toggle between hiding and showing the active panel */
-        var body = this.nextElementSibling;
-        if (body.style.display === "block") {
-            body.style.display = "none";
-        } else {
-            body.style.display = "block";
-        }
+$(document).ready(function() {
+    // $(".faqs-container .faq-singular:first-child").addClass("active").children(".faq-answer").slideDown();
+    $(".faq-question").on("click", function(){
+      if( $(this).parent().hasClass("active") ){
+        $(this).next().slideUp();
+        $(this).parent().removeClass("active");
+      }
+      else{
+        $(".faq-answer").slideUp();
+        $(".faq-singular").removeClass("active");
+        $(this).parent().addClass("active");
+        $(this).next().slideDown();
+      }
     });
-}
+  });
